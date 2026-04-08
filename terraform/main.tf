44d706444153821fbab8ecb2663bc2a267ac4d75
brendagold <petersbrenda47@gmail.com>
2026-04-07 21:38:53 -0400

    feat: add Terraform configuration for AWS infrastructure including security group, EC2 instance, and variables

---
+++ terraform/main.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "cartlift_sg" {
+  name        = "cartlift-sg"
+  description = "Security group for CartLift API"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "App port"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "cartlift_server" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.cartlift_sg.id]
+
+  tags = {
+    Name = "cartlift-server"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,21 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "Amazon Machine Image ID for EC2"
+  type        = string
+}
+
+variable "key_name" {
+  description = "EC2 key pair name"
+  type        = string
+}
